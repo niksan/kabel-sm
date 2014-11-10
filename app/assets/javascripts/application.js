f7733e00bf3191c4d65f2fedd66d7a653c047852
NikSan <nikulinaleksandr@gmail.com>
2014-11-10 22:47:38 +0300

    first steps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,33 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require html5
+//= require jquery.cycle.all.min
+//= require slides.min.jquery
 //= require_tree .
+
+$(function(){
+  $('#slides').slides({
+    preload: true,
+    preloadImage: 'img/loading.gif',
+    play: 5000,
+    pause: 2500,
+    hoverPause: true});});
+
+$(document).ready(function(){					
+    $("#back-top").hide();
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 200) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();}});
+        $('#back-top a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0}, 800);
+            return false;});});				
+  $("#slideshowCont").css("overflow", "hidden");
+  $("#last").css("display", "block");
+  $("#next").css("display", "block");
+  $('#slideShow ul').cycle({ fx: 'scrollLeft',pause:1, next: '#next', prev: '#last', timeout:0});}
+);
