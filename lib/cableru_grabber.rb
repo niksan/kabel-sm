af9924da39f164eb604d0951538a92a0da326256
NikSan <nikulinaleksandr@gmail.com>
2014-11-11 17:33:00 +0300

    upd

--- lib/cableru_grabber.rb
+++ lib/cableru_grabber.rb
@@ -2,26 +2,30 @@ module CableruGrabber
 
   DOMAIN='http://cable.ru'
   FIRST_ENTRIES = [
-    { type: :main, uri: '/' }#,
-#    { type: :group, uri: '/engines/' },
-#    { type: :group, '/pumps/' }
+#    { type: :main, uri: '/' },
+    { type: :group, uri: '/engines/' }#,
+#    { type: :group, uri: '/pumps/' }
   ]
   TYPES = {
-    main: { css_selector: '#content .column div.catalog.module' },
+    main: {
+      link_match: /@@!!@@!!\)\)XuiPizdaaa/,
+      li_match: /@@!!@@!!\)\)XuiPizdaaa/,
+      css_selector: '#content .column div.catalog.module'
+    },
     razdel: {
       link_match: /http:\/\/cable\.ru\/(\D)*\/(razdel-(\d)*\.php)/,
-      li_match: /<li><a href="(\/(\D)*\/(razdel-(\d)*\.php))" title="\W*">(\W*)<\/a><\/li>/,
-      css_selector: '#content .column2',
+      li_match: /<li><a href="((\/(\D)*\/)?(razdel-(\w)*\.php))"( title="\W*")?>(\W*)<\/a><\/li>/,
+      css_selector: '#content .column-2',
     },
     group: {
       link_match: /http:\/\/cable\.ru\/(\D)*\/(group-(\w)*\.php)/,
-      li_match: /<li><a href="(\/(\D)*\/(group-(\w)*\.php))" title="\W*">(\W*)<\/a><\/li>/,
-      selector_method: '#content .column2',
+      li_match: /<li><a href="((\/(\D)*\/)?(group-(\w)*\.php))"( title="\W*")?>(\W*)<\/a><\/li>/,
+      css_selector: '#content .column-2',
     },
     marka: {
       link_match: /http:\/\/cable\.ru\/(\D)*\/(marka-(\w)*\.php)/,
-      li_match: /<li><a href="(\/(\D)*\/(marka-(\w)*\.php))" title="\W*">(\W*)<\/a><\/li>/,
-      selector_method: '#content .column2',
+      li_match: /<li><a href="((\/(\D)*\/)?(marka-(\w)*\.php))"( title="\W*")?>(\W*)<\/a><\/li>/,
+      css_selector: '#content .column-2',
     }
   }
 
@@ -30,9 +34,9 @@ module CableruGrabber
     def grab(entries=FIRST_ENTRIES)
       start_time = Time.zone.now
       entries.each do |entry|
-        uri = DOMAIN + entry[:uri]
-        source = SimpleUri.req(uri)
-        puts get_links(source, entry[:type])
+        @uri = DOMAIN + entry[:uri]
+        source = SimpleUri.req(@uri)
+        puts get_links(source, entry[:type]).inspect
       end
       runtime = Time.zone.now - start_time
       puts "RUNTIME - #{ runtime }"
@@ -41,8 +45,18 @@ module CableruGrabber
     private
 
       def get_links(source, type)
+        result = []
         n = Nokogiri::HTML(source)
-        n.css TYPES[type][:css_selector]
+        part = n.css(TYPES[type][:css_selector]).to_s
+        TYPES.each do |type_name, type_params|
+          r = part.scan(type_params[:li_match]).map { |r| { uri: @uri+r[0], title: r[6] } }
+          result << r
+        end
+        result.select { |r| r.size > 2 }[0]
+      end
+
+      def detect_uri_type(uri)
+       # 
       end
     
   end
